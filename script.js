25ee1a9713c10eda21457352de67b0b9dcec7f25
dancastro18 <dancastro1802@gmail.com>
2025-06-03 16:41:49 -0600

    carga inicial

---
+++ script.js
@@ -0,0 +1,45 @@
+const contenedor = document.getElementById('contenedorDinamico');
+const cambiarContenidoBtn = document.getElementById('cambiarContenidoBtn');
+const crearElementoBtn = document.getElementById('crearElementoBtn');
+const cambiarEstiloBtn= document.getElementById('cambiarEstiloBtn');
+
+cambiarContenidoBtn.addEventListener('click',function(){
+    const parrafo = contenedor.querySelector('p');
+    if (parrafo) {
+        parrafo.textContent = 'El contenido de este parrafo ha sido modificado';
+        console.log('Contenido de parrafo cambiado');
+    }else{
+        contenedor.innerHTML = '<p> Parrafo insertado y creado desde JS</p>'
+    }
+});
+
+crearElementoBtn.addEventListener('click', function(){
+    const nuevoParrafo = document.createElement('p');
+    nuevoParrafo.textContent = 'Este es un parrafo creado dinamicamente';
+    nuevoParrafo.style.color = 'purple';
+    nuevoParrafo.style.fontWeight = 'bold';
+    contenedor.appendChild(nuevoParrafo);
+    console.log('Parrafo añadido dinamicamente');
+});
+
+const textoOriginal = contenedor.textContent; 
+const idOriginal = contenedor.id;
+let estiloOriginal = true;
+
+cambiarEstiloBtn.addEventListener('click',()=>{
+    if (estiloOriginal){
+        contenedor.textContent = 'Se ha cambiado el estilo';
+        contenedor.id = '';
+        contenedor.classList.toggle('estiloNuevo');
+        estiloOriginal = false;
+    }
+    else {
+        contenedor.textContent = textoOriginal;
+        contenedor.id = idOriginal;
+        contenedor.classList.remove('estiloNuevo');
+        estiloOriginal = true;
+    }
+});
+
+
+
